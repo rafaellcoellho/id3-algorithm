16be4cdedbc78b1c95a943306c3922db3346cdab
Rafael Coelho <rafaellcoellho@gmail.com>
2018-11-16 13:56:21 -0300

    Testa o id3

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 // console.log(entropy(datasetExample))
 // console.log(splitDataset(datasetExample, 0, 'overcast'))
-console.log(attributeNames[attributeSelection(datasetExample)])
+// console.log(attributeNames[attributeSelection(datasetExample)])
+console.log(id3(datasetExample, attributeNames))
+
