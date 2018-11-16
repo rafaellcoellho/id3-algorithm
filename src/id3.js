e1151d3f42a51b029cd702a8621165759ffb5fac
Rafael Coelho <rafaellcoellho@gmail.com>
2018-11-16 13:58:00 -0300

    Melhora indentação

--- src/id3.js
+++ src/id3.js
@@ -1,52 +1,52 @@
-function entropy (dataset) {
+function entropy(dataset) {
   const entries = dataset.length
   let labels = {}
   let entropy = 0.0
   let prob = 0.0
 
   dataset.forEach(data => {
-    let label = data[data.length-1]
+    let label = data[data.length - 1]
     if (labels.hasOwnProperty(label)) {
-      labels[label]++;
+      labels[label]++
     } else {
       labels[label] = 1
     }
   })
 
   for (let label of Object.keys(labels)) {
-    prob = labels[label]/entries
-    entropy -= prob*Math.log2(prob)
+    prob = labels[label] / entries
+    entropy -= prob * Math.log2(prob)
   }
 
   return entropy
 }
 
-function splitDataset (dataset, attributeIndex, attributeValue) {
+function splitDataset(dataset, attributeIndex, attributeValue) {
   let newDataset = []
-  dataset.forEach( data => {
+  dataset.forEach(data => {
     if (data[attributeIndex] === attributeValue) {
       let newData = data
-        .filter( data => data !== attributeValue)
+        .filter(data => data !== attributeValue)
       newDataset.push(newData)
     }
   })
   return newDataset
 }
 
-function attributeSelection (dataset) {
-  const numberOfAttributes = dataset[0].length-1
+function attributeSelection(dataset) {
+  const numberOfAttributes = dataset[0].length - 1
   const baseEntropy = entropy(dataset)
   let greaterInformationGain = 0.0
   let bestAttr
 
-  for (let i=0; i < numberOfAttributes; i++) {
+  for (let i = 0; i < numberOfAttributes; i++) {
     let labelsOfAttribute = {}
-    
-    dataset.forEach( data => {
+
+    dataset.forEach(data => {
       const label = data[i]
       if (!labelsOfAttribute.hasOwnProperty(label)) {
-        labelsOfAttribute[label] = 1;
-      } 
+        labelsOfAttribute[label] = 1
+      }
     })
 
     let attrEntropy = 0.0
@@ -57,7 +57,7 @@ function attributeSelection (dataset) {
       const newEntropy = prob * entropy(newData)
       attrEntropy += newEntropy
     })
-    
+
     const informationGain = baseEntropy - attrEntropy
     if (informationGain > greaterInformationGain) {
       greaterInformationGain = informationGain
@@ -66,20 +66,20 @@ function attributeSelection (dataset) {
   }
 
   return bestAttr
-} 
+}
 
-function id3 (dataset, labels) {
+function id3(dataset, labels) {
   const totalResultLabels = dataset
-    .map(data => data[data.length-1])
-    .length 
+    .map(data => data[data.length - 1])
+    .length
 
   const equalResultLabels = dataset
-    .map(data => data[data.length-1])
-    .filter(data => dataset[0][dataset[0].length-1] === data)
+    .map(data => data[data.length - 1])
+    .filter(data => dataset[0][dataset[0].length - 1] === data)
     .length
 
   if (totalResultLabels === equalResultLabels) {
-    return dataset[0][dataset[0].length-1]
+    return dataset[0][dataset[0].length - 1]
   }
 
   const maxGainNode = attributeSelection(dataset)
@@ -88,13 +88,13 @@ function id3 (dataset, labels) {
   let theTree = {}
   theTree[treeLabel] = {}
   delete labels[maxGainNode]
-  
-  let labelsOfAttribute = {}  
-  dataset.forEach( data => {
+
+  let labelsOfAttribute = {}
+  dataset.forEach(data => {
     const label = data[maxGainNode]
     if (!labelsOfAttribute.hasOwnProperty(label)) {
-      labelsOfAttribute[label] = 1;
-    } 
+      labelsOfAttribute[label] = 1
+    }
   })
 
   Object.keys(labelsOfAttribute).forEach(label => {
